87303ddb8a750414d540bcc35baaa34982c94ce4
Atila Zapico <zapicoatila00@gmail.com>
2023-02-15 23:00:26 -0300

    new back-end host

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://image-lsd.onrender.com/api/v1/post', {
+      const response = await fetch('https://lsd-images-production.up.railway.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
